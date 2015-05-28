6039279c162aed00ec89e332755ceecd46cb6cc6
d <dhenton@localhost.com>
2015-05-28 17:22:56 -0500

    art timer rewrite 5

--- showcase/arttimer/newArtTimer.js
+++ showcase/arttimer/newArtTimer.js
@@ -3,162 +3,54 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
- 
- 
+
+
+/* global d3 */
+
 var inAnimation = true;
-var timerVar = null;
 var imageList = null;
 var currentIdx = 0;
 var baseZIndex = 100;
 var midZIndex = 500;
 var topZIndex = 1000;
 var fadeLength = 1000;
-
-var tau = 2 * Math.PI;
-var radians = 0.0174532925;
-var clockRadius = 100;
-var margin = 50;
-var width = (clockRadius + margin) * 2;
-var height = (clockRadius + margin) * 2;
-var secondTickStart = clockRadius;
-var secondTickLength = -15;
-var secondLabelRadius = clockRadius + 16;
-var timerArc;
-var warnArray = ['green', 'greenyellow', 'gold', 'yellow', 'orangered', 'red'];
+var timerType = 0;
+var timerRunning = false;
 var svg;
-
-
-
-var radianScale = d3.scale.linear()
-        .range([0, 2 * Math.PI])
-        .domain([0, 60]);
-
-
-var arc = d3.svg.arc()
-        .innerRadius(0)
-        .outerRadius(clockRadius * .75)
-        .startAngle(0)
-//  .attr('transform', 'translate(' + (clockRadius/2) + ',' + (clockRadius/2) + ')');
-
+var initTimer = {"attachmentId": "#clockTimer", "margin": 50, "clockRadius": 100,"svg":svg};
+var timer;
 
 function init()
 {
     imageList = $('#imageCollection img');
     $(imageList[0]).fadeIn(10)
     $(imageList[0]).css("z-index", topZIndex);
-    drawClock();
+    timer = d3.artTimer.clock.init(initTimer);
 
 
 }
 
-
-function drawClock() {
-
-
-
-
-    var minuteScale = d3.scale.linear()
-            .range([0, 354])
-            .domain([0, 59]);
-
-    var secondScale = minuteScale;
-
-    svg = d3.select("#clockTimer").append("svg")
-            .attr("width", width)
-            .attr("height", height);
-
-    var face = svg.append('g')
-            .attr('id', 'clock-face')
-            .attr('transform', 'translate(' + (clockRadius + margin) + ',' + (clockRadius + margin) + ')');
-
-    //add marks for seconds
-    face.selectAll('.second-tick')
-            .data(d3.range(0, 30)).enter()
-            .append('line')
-            .attr('class', 'second-tick')
-            .attr('x1', 0)
-            .attr('x2', 0)
-            .attr('y1', secondTickStart)
-            .attr('y2', secondTickStart + secondTickLength)
-            .attr('transform', function (d) {
-                return 'rotate(' + secondScale(d) * 2 + ')';
-            });
-
-    face.selectAll('.second-label')
-            .data(d3.range(5, 61, 5))
-            .enter()
-            .append('text')
-            .attr('class', 'second-label')
-            .attr('text-anchor', 'middle')
-            .attr('x', function (d) {
-                return secondLabelRadius * Math.sin(secondScale(d) * radians);
-            })
-            .attr('y', function (d) {
-                return -secondLabelRadius * Math.cos(secondScale(d) * radians)
-            })
-            .text(function (d) {
-                return d;
-            });
-
-    timerArc = svg.append("path")
-            .datum({endAngle: (tau / 360)})
-            .style("fill", "#ddd")
-            .attr("d", arc)
-            .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
-
-}
-var secCounter = 0;
-var timeValue = 60;
-
-function doTimer()
+function changeTimer()
 {
-    if (secCounter === 0)
-    {
-        d3.timer(doSecTick(), 0, 1000);
-    }
-
-
-
+    timerType = (timerType + 1) % 3;
+    timer.setTimerLength(timerType);
 }
 
-function doSecTick()
+function toggleTimer(button)
 {
-    return function (  ) {
-
-        if (secCounter > timeValue)
-        {
-            secCounter = 0;
-            //broadcast the end of the timer run
-            return true;
-        }
-
-        timerArc.transition().call(arcTween, radianScale(secCounter));
-        secCounter = secCounter + 1;
-        d3.timer(doSecTick(), 1000);
-
-
-        return true;
-    }
-}
-
-function arcTween(transition, newAngle) {
-
-
-    transition.attrTween("d", function (d) {
-        var interpolate = d3.interpolate(d.endAngle, newAngle);
-        return function (t) {
-            d.endAngle = interpolate(t);
-            return arc(d);
-        };
-    }).each("end", function ()
+    if (timerRunning)
     {
-        var idx = Math.floor((secCounter * (warnArray.length-1))/ timeValue);
-        console.log(idx)
-        timerArc.transition().style("fill", warnArray[idx]);
-
-    });
+       timer.shutdown();
+       $(button).text("Start Timer");
+    }
+    else
+    {
+        timer.doTimer();
+        
+        $(button).text("Stop Timer");
+    }
+    timerRunning = !timerRunning;
 }
-
 
 function doFade()
 {
@@ -175,17 +67,17 @@ function doFade()
         {
             nextIdx = 0;
         }
-         
+
         var nextImage = $(imageList[nextIdx]);
         nextImage.css('z-index', midZIndex);
         topImage.fadeOut(fadeLength, function () {
             topImage.css('z-index', baseZIndex).hide();
             nextImage.css('z-index', topZIndex);
-             
+
         });
         nextImage.fadeIn(fadeLength, function ()
         {
-           // console.log("next done");
+            // console.log("next done");
         });
     }
 
--- showcase/arttimer/timer.js
+++ showcase/arttimer/timer.js
@@ -4,11 +4,13 @@
  * and open the template in the editor.
  */
 
+/* global d3 */
 
-d3.timer = {};
+d3.artTimer = {};
+d3.artTimer.clock = {};
 
 
-d3.indicator.init = function (initConditions)
+d3.artTimer.clock.init = function (initConditions)
 {
     var tau = 2 * Math.PI;
     var radians = 0.0174532925;
@@ -22,7 +24,12 @@ d3.indicator.init = function (initConditions)
     var timerArc;
     var warnArray = ['green', 'greenyellow', 'gold', 'yellow', 'orangered', 'red'];
     var secCounter = 0;
-    var timeValue = 60;
+    var timeLengthIdx = 0;
+    var timeMultiplier = [1, 2, 5];
+    var timeValue = [60 * 1, 60 * 2, 60 * 5];
+
+    var svg;
+    var attachmentId = initConditions.attachmentId;
 
     var radianScale = d3.scale.linear()
             .range([0, 2 * Math.PI])
@@ -34,15 +41,15 @@ d3.indicator.init = function (initConditions)
             .outerRadius(clockRadius * .75)
             .startAngle(0)
 
-    function drawClock() {
+    var drawClock = function () {
 
-        var minuteScale = d3.scale.linear()
+        var secondScale = d3.scale.linear()
                 .range([0, 354])
                 .domain([0, 59]);
 
-        var secondScale = minuteScale;
 
-        svg = d3.select("#clockTimer").append("svg")
+
+        svg = d3.select(attachmentId).append("svg")
                 .attr("width", width)
                 .attr("height", height);
 
@@ -73,21 +80,90 @@ d3.indicator.init = function (initConditions)
                     return secondLabelRadius * Math.sin(secondScale(d) * radians);
                 })
                 .attr('y', function (d) {
-                    return -secondLabelRadius * Math.cos(secondScale(d) * radians)
+                    return -secondLabelRadius * Math.cos(secondScale(d) * radians);
                 })
                 .text(function (d) {
-                    return d;
+                    return d * timeMultiplier[timeLengthIdx];
                 });
 
         timerArc = svg.append("path")
                 .datum({endAngle: (tau / 360)})
                 .style("fill", "#ddd")
                 .attr("d", arc)
-                .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+                .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+    };
+    var doTimer = function ()
+    {
+        if (isTimerStopped())
+        {
+            d3.timer(doSecTick(), 0, 1000);
+        }
+
+
+    };
+
+
+
+    var isTimerStopped = function ()
+    {
+        return (secCounter === 0)
+    }
+
+    var doSecTick = function ()
+    {
+        return function (  ) {
+
+            if (secCounter > timeValue[timeLengthIdx])
+            {
+                secCounter = 0;
+                //broadcast the end of the timer run
+                timerArc.transition().call(arcTween,
+                        radianScale(secCounter / timeMultiplier[timeLengthIdx]));
+                return true;
+            }
+
+            timerArc.transition().call(arcTween,
+                    radianScale(secCounter / timeMultiplier[timeLengthIdx]));
+            secCounter = secCounter + 1;
+            d3.timer(doSecTick(), 1000);
+
+
+            return true;
+        };
+    };
+
+    var arcTween = function (transition, newAngle) {
+
+
+        transition.attrTween("d", function (d) {
+            var interpolate = d3.interpolate(d.endAngle, newAngle);
+            return function (t) {
+                d.endAngle = interpolate(t);
+                return arc(d);
+            };
+        }).each("end", function ()
+        {    //* timeMultiplier[timeLengthIdx]
+            var idx = Math.floor((secCounter * (warnArray.length - 1)) / timeValue[timeLengthIdx]);
+
+            console.log("floor " + idx + " sec " + secCounter);
+            timerArc.transition().style("fill", warnArray[idx]);
+
+        });
+    };
+
+    var redrawClock = function ()
+    {
+        svg.selectAll('.second-label')
+                .data(d3.range(5, 61, 5))
+                .transition()
+                .text(function (d) {
+                    return d * timeMultiplier[timeLengthIdx];
+                });
+        timerArc.transition().call(arcTween, radianScale(secCounter));
 
     }
-   
-   
+
     drawClock();
     /////////// PUBLIC API//////////////////////////////////////////////
     function exports()
@@ -96,5 +172,27 @@ d3.indicator.init = function (initConditions)
     }
     ;
 
+    exports.shutdown = function ()
+    {
+        secCounter = secCounter + timeValue[timeLengthIdx];
+    }
+    exports.doTimer = function ()
+    {
+        doTimer();
+    };
+
+
+    /**
+     * 0 -- 60  sec
+     * 1 -- 120 sec
+     * 2 -- 300 sec 5 min
+     */
+    exports.setTimerLength = function (v)
+    {
+        timeLengthIdx = v;
+        redrawClock();
+    }
+
+
     return exports;
-}
+};
